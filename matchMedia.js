5f5a8a85d5cafad01780b59a2e6eab9caed76d0f
Jasmine Hegman <jasmine@webpt.com>
2013-02-19 13:12:54 -0700

    Removing extra newlines to make consistent with other script file

--- matchMedia.js
+++ matchMedia.js
@@ -1,5 +1,4 @@
 /*! matchMedia() polyfill - Test a CSS media type/query in JS. Authors & copyright (c) 2012: Scott Jehl, Paul Irish, Nicholas Zakas. Dual MIT/BSD license */
-
 window.matchMedia = window.matchMedia || (function( doc, undefined ) {
 
   "use strict";
@@ -32,5 +31,3 @@ window.matchMedia = window.matchMedia || (function( doc, undefined ) {
   };
 
 }( document ));
-
-
