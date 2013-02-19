e13f5ba6ef5da62b18fa43117bbfe5c6c22e4a55
Jasmine Hegman <jasmine@webpt.com>
2013-02-19 13:35:21 -0700

    Added basic logic for registration/de-registration of events

--- matchMedia.addListener.js
+++ matchMedia.addListener.js
@@ -8,7 +8,8 @@
 			var ret = oldMM( q ),
 				listeners = [],
 				last = false,
-				timer,
+				resizeListener,
+				reorientListener,
 				check = function(){
 					var list = oldMM( q ),
 						unmatchToMatch = list.matches && !last,
@@ -25,9 +26,30 @@
 			
 			ret.addListener = function( cb ){
 				listeners.push( cb );
-				if( !timer ){
-					timer = setInterval( check, 1000 );
+
+				// Activate resizer listener if necessary
+				if( !resizeListener ){
+					if(window.addEventListener) {
+						window.addEventListener('resize', check);
+					} else {
+						window.attachEvent('on' + 'resize', check);
+					}
+					resizeListener = true;
 				}
+
+				// Activate reorientation listener if necessary
+				// @TODO This is left commented out until I can verify it doesn't get called super often
+				// like accelerometer data might be
+				/*
+				if( !reorientListener ){
+					if(window.addEventListener) {
+						window.addEventListener('deviceorientation', check);
+					} else {
+						window.attachEvent('on' + 'deviceorientation', check);
+					}
+					reorientListener = true;
+				}
+				*/
 			};
 
 			ret.removeListener = function( cb ){
@@ -36,8 +58,27 @@
 						listeners.splice( i, 1 );
 					}
 				}
-				if( !listeners.length && timer ){
-					clearInterval( timer );
+				if( !listeners.length ){
+					// Remove resizer listener if necessary
+					if( resizeListener ){
+
+						if(window.removeEventListener) {
+							window.removeEventListener('resize', check);
+						} else {
+							window.detachEvent('on' + 'resize', check);
+						}
+						resizeListener = false;
+					}
+
+					// Remove reorientation listener if necessary
+					if( !reorientListener ){
+						if(window.removeEventListener) {
+							window.removeEventListener('deviceorientation', check);
+						} else {
+							window.detachEvent('on' + 'deviceorientation', check);
+						}
+						reorientListener = false;
+					}
 				}
 			};
 			
